24e516acc6ffe4d398c615ea53d6370e11343a53
Angel de Jesus Anguiano Camacho <ANGUIANOLAB2@GMAIL.COM>
2019-09-01 12:06:51 -0600

    then

---
+++ then.ts
@@ -0,0 +1,6 @@
+const	promise	=	new	Promise((resolve,	reject)	=>	{	
+                resolve(123);}); 
+    promise.then((res)	=>	{
+        console.log('I	get	called:',	res	===	123);	//	Devuelve:	true 
+    }); promise.catch((err)	=>	{				//	Nuca	es	utilizado 
+    });
