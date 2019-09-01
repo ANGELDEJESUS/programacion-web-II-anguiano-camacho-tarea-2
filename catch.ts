23d8e8d2a792503fa2802e13485778de2b645b99
Angel de Jesus Anguiano Camacho <ANGUIANOLAB2@GMAIL.COM>
2019-09-01 12:06:20 -0600

    then

---
+++ catch.ts
@@ -0,0 +1,6 @@
+const	promise	=	new	Promise((resolve,	reject)	=>	{
+        reject(new	Error("Algo	malo	a	pasado")); 
+    }); promise.then((res)	=>	{				//	This	is	never	called
+     }); promise.catch((err)	=>	{	
+console.log('Tengo	una	llamada:	',	err.message);	//	Tengo	una	llamada:	'Algo	malo	a	pasado'
+ });
