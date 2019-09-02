4b2e1e80a6f09a65066b86d5fe0c28ddde7cbc5c
Angel de Jesus Anguiano Camacho <ANGUIANOLAB2@GMAIL.COM>
2019-09-01 22:22:25 -0600

    cadenas de promesas

---
+++ cadenasprom.js
@@ -0,0 +1,9 @@
+Promise.resolve(123).then(function (res) {
+    throw new Error('something	bad	happened');
+    return 456;
+}).then(function (res) {
+    console.log(res); //	never	called	
+    return Promise.resolve(789);
+})["catch"](function (err) {
+    console.log(err.message); //	something	bad	happened				
+});
---
+++ cadenasprom.ts
@@ -0,0 +1,9 @@
+Promise.resolve(123).then((res)	=>	{
+    throw	new	Error('something	bad	happened')								
+    return	456;				
+}).then((res)	=>	{
+console.log(res);	//	never	called	
+return	Promise.resolve(789);			
+}).catch((err)	=>	{
+console.log(err.message);	//	something	bad	happened				
+})
